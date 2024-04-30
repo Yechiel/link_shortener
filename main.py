748a46310d99945b3221e7a97e72f53f35372eb8
Yechiel <hilik@campus.technion.ac.il>
2024-04-30 23:28:37 +0300

    new api

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from link_shortner import LinkShortner
 
+
+from link_shortner import LinkShortner
+'''
 urls=[
     "https://walla.co.il",
     "https://ynet.co.il",
@@ -7,3 +9,19 @@ urls=[
 ]
 
 id=LinkShortner.shorten(urls[2])
+'''
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+app=FastAPI()
+@app.get("/{id}}")
+def health_check(id: str):
+    #return {"status": id} 
+    url = LinkShortner.get_url_by_id(id)
+    return RedirectResponse(url)
+
+@app.post("/shorten")
+def creat_url(params: dict):
+    url=params["url"]
+    id=LinkShortner.shorten(url)
+    return {"id":id}
