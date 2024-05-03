1de3cbbc84d79489638b1e635023225a4db6e6a5
Yechiel <hilik@campus.technion.ac.il>
2024-05-03 13:49:52 +0300

    add scripts

---
+++ kill_server.sh
@@ -0,0 +1,3 @@
+kill $(lsof -t -i :8000)
+kill $(lsof -t -i :8001)
+kill $(lsof -t -i :8003)
--- main.py
+++ main.py
@@ -16,9 +16,9 @@ from fastapi.responses import RedirectResponse
 app=FastAPI()
 @app.get("/api/{id}")
 def health_check(id: str):
-    return {"status": id} 
-    #url = LinkShortner.get_url_by_id(id)
-    #return RedirectResponse(url)
+    #return {"status": id} 
+    url = LinkShortner.get_url_by_id(id)
+    return RedirectResponse(url)
 
 @app.post("/api/shorten")
 def creat_url(params: dict):
