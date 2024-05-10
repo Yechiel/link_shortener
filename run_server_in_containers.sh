025d487800ea49b10d696201d56443e60227f766
Yechiel <hilik@campus.technion.ac.il>
2024-05-10 12:12:18 +0300

    add mounting func

--- run_server_in_containers.sh
+++ run_server_in_containers.sh
@@ -1,5 +1,5 @@
-docker run -p 8001:8080 link-shortner-dev &
-docker run -p 8002:8080 link-shortner-dev &
-docker run -p 8003:8080 link-shortner-dev &
+docker run -p 8001:8080 -v ./data/:/app/data link-shortner-dev &
+docker run -p 8002:8080 -v ./data/:/app/data link-shortner-dev &
+docker run -p 8003:8080 -v ./data/:/app/data link-shortner-dev &
 
 wait
