c0fbc5b1ec1d75be3bf20c7fbefe96305294ef21
Yechiel <hilik@campus.technion.ac.il>
2024-05-17 14:21:50 +0300

    correct path

--- run_deletion_docker.sh
+++ run_deletion_docker.sh
@@ -1,2 +1,2 @@
-  docker compose up delete_invalid_links
+  docker compose -f /home/cyber/projects/link_shortner/docker-compose.yml up delete_invalid_links
   #docker run -e LINKS_PATH=./data/links.json -v ./data/:/app/data/ link-shortner-devops -c "python delete_invalid_links.py"
